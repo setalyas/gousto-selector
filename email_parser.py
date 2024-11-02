b7c086f0728c9b2c7d47d39064e10ba96e47fc1d
Sami Capulcu <setatimmi@gmail.com>
2024-11-02 21:34:11 +0000

    Correct delivery dates

--- email_parser.py
+++ email_parser.py
@@ -11,6 +11,7 @@ from os.path import isfile, join
 import re
 import random
 import dateutil.parser as dparser
+import datetime as dt
 
 #%% Get list off emails
 
@@ -42,17 +43,26 @@ for i, f in enumerate(onlyfiles):
     # not multipart - i.e. plain text, no attachments, keeping fingers crossed
     else:
         body = msg.get_payload(decode=True)
-    bodies.append(body)
+    
+    # Get the date
     date_str = email.utils.parsedate_to_datetime(msg['date']).strftime('%Y-%m-%d')
     print(f'{i}: {date_str}')
+    
+    # Store
+    bodies.append((body, date_str))
 
 bodies.pop(128)  # Accidentally booked a 2-meal box!
 
 #%% Parse email
 
-body_cleaned = []
+def year_swap(d, year):
+    return dt.datetime(year, d.month, d.day)
 
-for body in bodies:
+body_cleaned = {}
+
+for el in bodies:
+    body = el[0]
+    send_date = el[1]
     b = body.decode("utf-8")
     b = b.replace('\xad', '')
     b = b.replace('\u00ad', '')
@@ -61,7 +71,24 @@ for body in bodies:
     # bodys = bodys.replace('\n\n', '\n')
     # bodys = re.sub('\n\s+\n', '\n', bodys)
     # bodys = re.sub(r'\s{5,}', '    ', bodys)
-    body_cleaned.append(b)
+    # body_cleaned.append(b)
+    
+    # Get the delivery date
+    b_oneline = b.replace('\n', ' ')
+    b_oneline = b_oneline.replace('  ', ' ')
+    date_str_rgx = r"(delivered|arriving) on ([\w]+ \d{1,2}(rd|st|nd|th)?([\s]?[\d]{4})?[\s]?[\w]+)"
+    date_surround = re.search(date_str_rgx, b_oneline).group(2)
+    parsed_date = dparser.parse(date_surround, fuzzy=True)
+    # delivery_date = year_swap(parsed_date, int(send_date[:4]))
+    # print(send_date, delivery_date.strftime('%Y-%m-%d'))
+    delivery_date = None
+    for year in range(2019, 2024+1):
+        delivery_option = year_swap(parsed_date, year)
+        diff = (dparser.parse(send_date) - delivery_option).days
+        if abs(diff) < 50:
+            delivery_date = delivery_option
+    # print(send_date, delivery_date.strftime('%Y-%m-%d'))
+    body_cleaned[delivery_date.strftime('%Y-%m-%d')] = b
 
 #%% Get relevant lines
 
@@ -71,7 +98,7 @@ for body in bodies:
 
 counter = 0
 
-titles = []
+titles = {}
 
 replacers = {'’': '',
              "'": '',
@@ -85,7 +112,7 @@ replacers = {'’': '',
              "Peri-nnaise": "Perinnaise",
              "Meat-free": "Meat-Free"}
 
-for i, b in enumerate(body_cleaned):
+for d, b in body_cleaned.items():
     lines = b.split('\n')
     # Remove trip hazards
     for k, v in replacers.items():
@@ -97,9 +124,9 @@ for i, b in enumerate(body_cleaned):
     # https://www.geeksforgeeks.org/python-filter-a-list-based-on-the-given-list-of-strings/
     title_clean = [b for b in title if all(a not in b for a in drop_strings)]
     if len(title_clean) != 4 and len(title_clean) != 5:
-        print(i, title_clean)
+        print(d, title_clean)
         counter += 1
-    titles.append(title_clean)
+    titles[d] = title_clean
 
 print(f'{counter} weeks with issues')
 
