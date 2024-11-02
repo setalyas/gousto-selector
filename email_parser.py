3337c198d76c2d4bc109baa27c4fc4d2ad6b60e0
Sami Capulcu <setatimmi@gmail.com>
2024-11-02 16:51:13 +0000

    Working list

--- email_parser.py
+++ email_parser.py
@@ -9,7 +9,8 @@ import email
 from os import listdir
 from os.path import isfile, join
 import re
-from collections import Counter
+import random
+import dateutil.parser as dparser
 
 #%% Get list off emails
 
@@ -21,7 +22,7 @@ onlyfiles = [f for f in listdir(folder) if isfile(join(folder, f))]
 
 bodies = []
 
-for f in onlyfiles:
+for i, f in enumerate(onlyfiles):
     fp = f'RawData\\mails\\{f}'
     
     with open(fp, 'r') as f:
@@ -42,6 +43,10 @@ for f in onlyfiles:
     else:
         body = msg.get_payload(decode=True)
     bodies.append(body)
+    date_str = email.utils.parsedate_to_datetime(msg['date']).strftime('%Y-%m-%d')
+    print(f'{i}: {date_str}')
+
+bodies.pop(128)  # Accidentally booked a 2-meal box!
 
 #%% Parse email
 
@@ -64,18 +69,42 @@ for body in bodies:
 # dupes = [k for k,v in Counter(lines).items() if v>1]
 # dupes = [i for i in dupes if not re.search(r'^(\s+)?$', i)]
 
-# Meat-Free --> Meat Free
-# Remove ’'- 
-# lower case 'with'
-# BBQ
+counter = 0
+
+titles = []
+
+replacers = {'’': '',
+             "'": '',
+             " with": " With ",
+             " x ": " X ",
+             "BBQ": "Bbq",
+             "DIY": "Diy",
+             "TABASCO®": "Tabasco",
+             " (DF)": "",
+             "Squash-age": "Squashage",
+             "Peri-nnaise": "Perinnaise",
+             "Meat-free": "Meat-Free"}
 
 for i, b in enumerate(body_cleaned):
     lines = b.split('\n')
+    # Remove trip hazards
+    for k, v in replacers.items():
+        lines = [s.replace(k, v) for s in lines]
     title = [s for s in lines if s.istitle()]
     title = list(set(title))
     drop_strings = ['Google Play', 'Total: ', 'App Store', '©', 'Inc.',
                     'Box Price', 'Hi Sami', 'Your Gousto Team']
     # https://www.geeksforgeeks.org/python-filter-a-list-based-on-the-given-list-of-strings/
     title_clean = [b for b in title if all(a not in b for a in drop_strings)]
-    if len(title_clean) != 4:
+    if len(title_clean) != 4 and len(title_clean) != 5:
         print(i, title_clean)
+        counter += 1
+    titles.append(title_clean)
+
+print(f'{counter} weeks with issues')
+
+mega_list = [t for t_list in titles for t in t_list]
+
+#%% Get random meal
+
+random.choices(mega_list, k=10)
